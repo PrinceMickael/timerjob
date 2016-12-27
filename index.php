111a55b4229fc1d2b32f0ae4c57a52a3f698ed51
Prince <princemickaellouis@gmail.com>
2016-12-27 15:05:15 +0100

    index.php v2

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Timer Job</title>
+      <link rel="stylesheet" href="css/bootstrap.css">
+      <link rel="stylesheet" href="css/styleindex.css">
+  </head>
+
+
+  <body>
+    <script type="text/javascript" href="bootstrap.js"></script>
+    <header>
+      <div class="container-fluid">
+        <h3>Timer Job</h3>
+      </div>
+    </header>
+
+    <main>
+      <div class="container-fluid">
+        <strong><a href="connexion.php"><button type="button" class="connexion"  name="connexion">Se connecter</button></a></strong>
+        <strong><a href="register.php"><button type="button" class="register" name="register">S'inscrire</button></a></strong>
+      </div>
+    </main>
+
+    <footer>
+    </footer>
+  </body>
+</html>
