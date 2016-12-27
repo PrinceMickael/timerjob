ff5ed2ff353ae9ea52dc1752badbb9af49d24292
hayden21 <lesale.zakia@gmail.com>
2016-12-27 18:07:14 +0100

    index.php v3

--- index.php
+++ index.php
@@ -1,26 +1,79 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Timer Job</title>
-      <link rel="stylesheet" href="css/bootstrap.css">
-      <link rel="stylesheet" href="style.css">
+ <head>
+   <meta charset="utf-8">
+   <title>Timer Job</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+     <link rel="stylesheet" href="css/bootstrap.css">
+     <link rel="stylesheet" href="styleindex.css">
 
-  </head>
-  <body>
-    <script type="text/javascript" href="bootstrap.js"></script>
-    <header>
-      <div class="container-fluid">
-        <h3>Timer Job</h3>
+
+ </head>
+
+ <body>
+
+   <header>
+     <div class="container-fluid">
+       <h3>Timer Job</h3>
 </div>
-    </header>
-    <div class="container-fluid">
-      <strong><a href="connexion.php"><button type="button" class="connexion"  name="connexion">Se connecter</button></a></strong>
-    <strong>  <a href="register.php"><button type="button" class="register" name="register">S'inscrire</button></a></strong>
+   </header>
+   <div class="container-fluid">
+
+     <strong><button type="button" class="connexion" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal"  name="connexion">Se connecter</button></a></strong>
+   <strong>  <button type="button" class="register" class="btn btn-primary btn-lg" name="register" data-toggle="modal" data-target="#myModa2">S'inscrire</button></a></strong>
 </div>
-  </body>
-  <footer>
 
+<!-- Modal 1 -->
 
-  </footer>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+ <div class="modal-dialog" role="document">
+   <div class="modal-content">
+     <div class="modal-header">
+       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+         <span aria-hidden="true">&times;</span>
+       </button>
+       <h4 class="modal-title" id="myModalLabel">Se connecter</h4>
+     </div>
+     <div class="modal-body">
+       ...
+     </div>
+     <div class="modal-footer">
+       <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       <button type="button" class="btn btn-primary">Save changes</button>
+     </div>
+   </div>
+ </div>
+</div>
+
+<!-- Modal 2  -->
+
+<div class="modal fade" id="myModa2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+ <div class="modal-dialog" role="document">
+   <div class="modal-content">
+     <div class="modal-header">
+       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+         <span aria-hidden="true">&times;</span>
+       </button>
+       <h4 class="modal-title" id="myModalLabel">S'inscrire</h4>
+     </div>
+     <div class="modal-body">
+       ...
+     </div>
+     <div class="modal-footer">
+       <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       <button type="button" class="btn btn-primary">Save changes</button>
+     </div>
+   </div>
+ </div>
+</div>
+
+<script type="text/javascript" href="bootstrap.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+
+<footer>
+
+</footer>
+ </body>
 </html>
